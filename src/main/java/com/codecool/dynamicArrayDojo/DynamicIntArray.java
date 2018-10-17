7e40d95f5c2e182ae3bc4b601829ada709ae5675
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-17 18:16:00 +0200

    Change constructors to initialize indexes used field

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,20 +3,24 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
-    private static final int STANDARD_SIZE = 0;
+    private static final int STARTING_ARRAY_SIZE = 0;
+    private static final int STARTING_INDEXES_USED_WITH_ZERO_SIZE = 0;
 
     private int[] array;
-    private int[] indexesUsed = new int[0];
+    private int[] indexesUsed;
 
     public DynamicIntArray() {
-        this.array = new int[STANDARD_SIZE];
+        this.array = new int[STARTING_ARRAY_SIZE];
+        this.indexesUsed = new int[STARTING_INDEXES_USED_WITH_ZERO_SIZE];
     }
 
     public DynamicIntArray(int size) throws NegativeArraySizeException {
         if (size < 0) {
             throw new NegativeArraySizeException();
         }
+
         this.array = new int[size];
+        this.indexesUsed = new int[size];
     }
 
     @Override
