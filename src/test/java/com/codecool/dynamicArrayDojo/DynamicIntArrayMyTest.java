41a299140b76d790d9e58d424bc3c0654def6126
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-17 16:42:00 +0200

    Add test checking return value of toString() method when size passed

--- src/test/java/com/codecool/dynamicArrayDojo/DynamicIntArrayMyTest.java
+++ src/test/java/com/codecool/dynamicArrayDojo/DynamicIntArrayMyTest.java
@@ -42,6 +42,16 @@ class DynamicIntArrayMyTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void returns_valid_string_when_only_size_passed() {
+        DynamicIntArray array = this.initializeArray(50);
+
+        String expected = " ";
+        String actual = array.toString();
+
+        assertEquals(expected, actual);
+    }
+
     private DynamicIntArray initializeArray() {
         return new DynamicIntArray();
     }
