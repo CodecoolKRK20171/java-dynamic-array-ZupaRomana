d9bd7a5a0bc35a165777e6533d8c8c8264a7a2a0
ZupaRomana <adam.romanowski93@gmail.com>
2018-10-17 15:42:34 +0200

    Temporary comment all - tests in second class can not run

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -3,4 +3,10 @@ package com.codecool.dynamicArrayDojo;
 // put your code here!
 public class DynamicIntArray {
 
+    private static final int STANDARD_SIZE = 20;
+    private int[] array;
+
+    public DynamicIntArray() {
+        this.array = new int[STANDARD_SIZE];
+    }
 }
